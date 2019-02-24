f642a293eb13d86c8528aa1f0ef871dbbf8f8238
Ignacio Tolstoy <arkantos798@gmail.com>
2019-02-24 15:33:38 +0100

    Part 3 & 4

--- 01-first-game/game.js
+++ 01-first-game/game.js
@@ -8,11 +8,18 @@ var config = {
     type: Phaser.AUTO,
     width: WIDTH,
     height: HEIGHT,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        }
+    },
     scene: {
         preload: preload,
         create: create,
-        update: update
-    }
+        update: update,
+    },
 }
 
 var game = new Phaser.Game(config)
@@ -25,11 +32,20 @@ function preload() {
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
 }
 
+var platforms
+
 function create() {
     // Objects are positioned based on the center by default.
     // setOrigin changes their anchor point.
     this.add.image(0, 0, 'sky').setOrigin(0, 0)
-    this.add.image(MIDDLE_WIDTH, MIDDLE_HEIGHT, 'star')
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(MIDDLE_WIDTH, 568, 'ground').setScale(2).refreshBody()
+
+    platforms.create(600, 400, 'ground')
+    platforms.create(50, 250, 'ground')
+    platforms.create(750, 220, 'ground')
 }
 
 function update() {
