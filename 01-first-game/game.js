a64c8ec2035b02a46e41424675f9070d14597057
Ignacio Tolstoy <arkantos798@gmail.com>
2019-02-24 15:46:03 +0100

    Part 5

--- 01-first-game/game.js
+++ 01-first-game/game.js
@@ -24,6 +24,9 @@ var config = {
 
 var game = new Phaser.Game(config)
 
+var platforms
+var player
+
 function preload() {
     this.load.image('sky', 'assets/sky.png')
     this.load.image('ground', 'assets/platform.png')
@@ -31,8 +34,6 @@ function preload() {
     this.load.image('bomb', 'assets/bomb.png')
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 })
 }
-
-var platforms
 
 function create() {
     // Objects are positioned based on the center by default.
@@ -42,10 +43,33 @@ function create() {
     platforms = this.physics.add.staticGroup();
 
     platforms.create(MIDDLE_WIDTH, 568, 'ground').setScale(2).refreshBody()
-
     platforms.create(600, 400, 'ground')
     platforms.create(50, 250, 'ground')
     platforms.create(750, 220, 'ground')
+
+    player = this.physics.add.sprite(100, 450, 'dude')
+    player.setBounce(0.2)
+    player.setCollideWorldBounds(true)
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1,
+    })
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'dude', frame: 4}],
+        frameRate: 20,
+    })
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1,
+    })
 }
 
 function update() {
