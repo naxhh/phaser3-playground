31b042479f71aaae2b6fc54dcb195f4616282de3
Ignacio Tolstoy <arkantos798@gmail.com>
2019-02-24 16:15:20 +0100

    Part 10 - final

--- 01-first-game/game.js
+++ 01-first-game/game.js
@@ -26,11 +26,14 @@ var game = new Phaser.Game(config)
 
 var platforms
 var stars
+var bombs
 var player
 var cursors
-
-var score = 0
 var scoreText
+
+var gameOver = false
+var score = 0
+
 
 function preload() {
     this.load.image('sky', 'assets/sky.png')
@@ -93,6 +96,11 @@ function create() {
     this.physics.add.collider(stars, platforms)
     this.physics.add.overlap(player, stars, collectStar, null, this)
 
+    bombs = this.physics.add.group()
+
+    this.physics.add.collider(bombs, platforms)
+    this.physics.add.collider(player, bombs, hitBomb, null, this)
+
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000'})
 }
 
@@ -120,4 +128,28 @@ function collectStar(player, star) {
 
     score += 10
     scoreText.setText('Score: ' + score)
+
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate((child) => {
+            child.enableBody(true, child.x, 0, true, true)
+        })
+
+        var x = (player.x < MIDDLE_WIDTH) ?
+            Phaser.Math.Between(MIDDLE_WIDTH, WIDTH) :
+            Phaser.Math.Between(0, MIDDLE_WIDTH)
+
+        var bomb = bombs.create(x, 16, 'bomb')
+        bomb.setBounce(1)
+        bomb.setCollideWorldBounds(true)
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+    }
+}
+
+function hitBomb(player, bomb) {
+    this.physics.pause()
+
+    player.setTint(0xff0000)
+    player.anims.play('turn')
+
+    gameOver = true
 }
